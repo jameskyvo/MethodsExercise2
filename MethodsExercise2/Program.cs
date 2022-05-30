71c5fc8c09fa8fcce01417f0b36cd161e361b401
gamejunkiez <queenangrylobster@gmail.com>
2022-05-30 12:29:39 -0500

    added basic operation methods

---
+++ MethodsExercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿static int Addition(int numOne, int numTwo)
+{
+    return numOne + numTwo;
+}
+
+static int Subtraction(int numOne, int numTwo)
+{
+    return numOne - numTwo;
+}
+
+static int Multiplication(int numOne, int numTwo)
+{
+    return numOne * numTwo;
+}
+
+static decimal Division(decimal numOne, decimal numTwo)
+{
+    return numOne / numTwo;
+}
+
+
+Console.WriteLine(Addition(1, 2));
+Console.WriteLine(Subtraction(1, 2));
+Console.WriteLine(Multiplication(1, 2));
+Console.WriteLine(Division(1, 2));
